cff0cfbb955df64d118d4dfb3af397281520a665
KrishanSaini01 <krishansaini80150@gmail.com>
2026-06-29 16:05:31 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask,render_template, url_for,request
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/history')
+def history():
+    return render_template('history.html')
+
+@app.route('/project')
+def project():
+    return render_template('project.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    return "Predict route working"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
